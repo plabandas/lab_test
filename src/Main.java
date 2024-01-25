dbebf959fb9b86f690d96dbb63901a27ec429d00
Plaban <turjoyplaban09@gmail.com>
2024-01-25 15:10:18 +0600

    Provided Patern

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape1 = shapeFactory.getShape("SQUARE");shape1.draw();
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");shape2.draw();
+    }
+}
